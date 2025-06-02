b27dd47a0ef4095c151a09388b7473811b63df8d
shadowy-pycoder <35629483+shadowy-pycoder@users.noreply.github.com>
2025-06-02 17:08:14 +0300

    Removed one line

--- gohpts.go
+++ gohpts.go
@@ -214,7 +214,6 @@ func (p *proxyApp) handleForward(w http.ResponseWriter, r *http.Request) {
 	n, err := io.Copy(w, resp.Body)
 	if err != nil {
 		p.logger.Error().Err(err).Msgf("Error during Copy() %s: %s", r.URL.String(), err)
-		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 	var written string
